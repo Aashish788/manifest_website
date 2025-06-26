d6c28c69f140cd2d19aa1aa3f08a55a71a450d0c
aashish <aashish.choudhary78892@gmail.com>
2025-06-26 15:05:19 +0530

    feat: Add Vercel Analytics

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { Analytics } from '@vercel/analytics/react';
 import Home from './pages/Home';
 import VisionBoard from './pages/VisionBoard';
 
@@ -10,6 +11,7 @@ function App() {
         <Route path="/vision-board" element={<VisionBoard />} />
         {/* Add more routes as needed */}
       </Routes>
+      <Analytics />
     </Router>
   );
 }
